7daf7a6e2a2600f9f61b9065eb2d48bc34cec444
Andres <andresfreites168@gmail.com>
2025-10-23 14:56:20 -0400

    actualizacion Arrays

--- app/src/main/java/com/freites/superandroidmaster/Arrays.kt
+++ app/src/main/java/com/freites/superandroidmaster/Arrays.kt
@@ -1,2 +1,30 @@
 package com.freites.superandroidmaster
 
+fun main(){
+    var name: String = "Andres"
+    var name1: String = "Andres"
+    var name2: String = "Andres"
+    var name3: String = "Andres"
+
+    val weekDays= arrayOf("lunes", "Martes","Miercoles","Jueves","Viernes","Sabado","Domingo" )
+
+    println(weekDays.size)
+
+    if (weekDays.size >=8) {
+        println(weekDays[7])
+    }else{
+        //println("No hay mas valores en el Array")
+    }
+//Modificar valores
+weekDays[0] = "Hi"
+    //println(weekDays[0])
+
+    // Bucles para Arrays
+for (position in weekDays.indices){
+    //println(weekDays[position])
+}
+    for ((position, value) in weekDays.withIndex()){
+        println("la posicion $position, contiene $value")
+    }
+    }
+
