cdc096691e03c0f22401b656810ea72d5c72f486
Andres <andresfreites168@gmail.com>
2025-11-10 14:01:36 -0500

    Empezando TodoApp

--- app/src/main/java/com/freites/superandroidmaster/sintaxis/firstapp/MenuActivity.kt
+++ app/src/main/java/com/freites/superandroidmaster/sintaxis/firstapp/MenuActivity.kt
@@ -9,6 +9,7 @@ import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.freites.superandroidmaster.R
 import com.freites.superandroidmaster.sintaxis.imccalucalator.imcCalculatorActivity
+import com.freites.superandroidmaster.sintaxis.todoapp.TodoActivity
 
 class MenuActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -17,14 +18,21 @@ class MenuActivity : AppCompatActivity() {
         setContentView(R.layout.activity_menu)
 val btnSaludApp = findViewById<Button>(R.id.btnSaludApp)
         val btnIMCApp = findViewById<Button>(R.id.btnIMCApp)
+        val btnTODO = findViewById<Button>(R.id.btnTODO)
         btnSaludApp.setOnClickListener { navigateToSaludApp() }
 btnIMCApp.setOnClickListener { navigateToIMCApp() }
+        btnTODO.setOnClickListener { navigateToTodoApp() }
     }
      private fun navigateToSaludApp(){
         val intent = Intent(this, FirstAppActivity::class.java)
         startActivity(intent)
 
     }
+}
+
+private fun MenuActivity.navigateToTodoApp() {
+    val intent = Intent (this, TodoActivity::class.java)
+    startActivity(intent)
 }
 
 private fun MenuActivity.navigateToIMCApp() {
---
+++ app/src/main/java/com/freites/superandroidmaster/sintaxis/todoapp/TodoActivity.kt
@@ -0,0 +1,19 @@
+package com.freites.superandroidmaster.sintaxis.todoapp
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.freites.superandroidmaster.R
+
+class TodoActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_todo)
+
+
+
+    }
+}
